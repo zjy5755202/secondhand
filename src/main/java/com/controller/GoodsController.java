86fd9d1812fdb9c578a6b20992c1abbf575854e1
huhaomeng <emhhm@outlook.com>
2019-05-15 09:46:09 +0800

    用户头像更新 商品、供需封面更新

--- src/main/java/com/controller/GoodsController.java
+++ src/main/java/com/controller/GoodsController.java
@@ -6,14 +6,21 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.entity.Goods;
 import com.entity.GoodsDetails;
+import com.entity.User;
 import com.redis.Redis;
 import com.service.GoodsService;
+import com.util.UUIDGenerrator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -25,6 +32,8 @@ public class GoodsController {
     private GoodsService goodsService;
     @Autowired
     private Redis redis;
+
+    private String coverPath = "./img/GoodPath/";
 
     //首页加载的时候按照时间来挑选一定的goods
     @RequestMapping("/queryGoodsByTime")
@@ -126,6 +135,31 @@ public class GoodsController {
         return result;
     }
 
+
+    /**商品封面改变
+     * @param request
+     * @param response
+     * @param cover 封面图片文件
+     * @throws IOException
+     */
+    @RequestMapping("/updateCover")
+    public void updateCover(HttpServletRequest request, HttpServletResponse response, @RequestParam(value = "cover", required = false) MultipartFile cover) throws IOException {
+        //判断图片文件是否存在
+        if (cover != null){
+            String fileName = cover.getOriginalFilename();
+            String type = fileName.indexOf(".") != -1 ? fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length()) : null;
+            if ("GIF".equals(type.toUpperCase())||"PNG".equals(type.toUpperCase())||"JPG".equals(type.toUpperCase())) {
+                String path = coverPath + '/' + UUIDGenerrator.getUUID() + '.' + type;
+                //保存头像文件
+                cover.transferTo(new File(path));
+                String goodUUID = request.getParameter("goodUUID");
+                Goods good = goodsService.queryGoodsById(goodUUID);
+                //更新数据库存储的路径
+                good.setCover(path);
+                goodsService.updateGoods(good);
+            }
+        }
+    }
 
 
 }
